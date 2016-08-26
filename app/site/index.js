73a7b76273d96b6ff0033f540877a673bd9c94dc
Sergey Ryzhov <sergey.ryzhov@firstlinesoftware.com>
2016-08-26 13:35:38 +0300

    cleanup

--- app/site/index.js
+++ app/site/index.js
@@ -14,7 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.ico')));
 app.use(webLogger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
